184139e8f36fd1fd1a3f80e3e8535e8ff2b2b198
Ioannis Maliouris <giannism@e-avenue.eu>
2024-10-15 00:00:37 +0300

    chore: close mobile menu on successful navigation

--- resources/js/Layouts/ParentPanelLayout.vue
+++ resources/js/Layouts/ParentPanelLayout.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import {Baby, BookOpen, Home, LogOut, Menu} from 'lucide-vue-next'
+import {Baby, BookOpen, LogOut, Menu} from 'lucide-vue-next'
 import {Button} from '@/components/ui/button'
 import {Sheet, SheetContent, SheetTrigger} from '@/components/ui/sheet'
 import {MenuItem} from "@/components/ui/menu-item";
 import {Link} from '@inertiajs/vue3';
+import {ref} from "vue";
+
+const sheetOpen = ref(false)
 </script>
 
 <template>
@@ -12,31 +15,32 @@ import {Link} from '@inertiajs/vue3';
             <div class="flex h-full max-h-screen flex-col gap-2">
                 <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
                     <a href="/" class="flex items-center gap-2 font-semibold">
-<!--                        <Baby class="h-6 w-6" />-->
-                        <span class="">{{$page.props.appName}}</span>
+                        <!--                        <Baby class="h-6 w-6" />-->
+                        <span class="">{{ $page.props.appName }}</span>
                     </a>
-<!--                    <Button variant="outline" size="icon" class="ml-auto h-8 w-8">-->
-<!--                        <Bell class="h-4 w-4" />-->
-<!--                        <span class="sr-only">Toggle notifications</span>-->
-<!--                    </Button>-->
+                    <!--                    <Button variant="outline" size="icon" class="ml-auto h-8 w-8">-->
+                    <!--                        <Bell class="h-4 w-4" />-->
+                    <!--                        <span class="sr-only">Toggle notifications</span>-->
+                    <!--                    </Button>-->
                 </div>
                 <div class="flex-1">
                     <nav class="grid items-start px-2 text-sm font-medium lg:px-4">
-<!--                        <MenuItem-->
-<!--                            route-name="dashboard"-->
-<!--                        >-->
-<!--                            <Home class="h-4 w-4" />-->
-<!--                            Dashboard-->
-<!--                        </MenuItem>-->
+                        <!--                        <MenuItem-->
+                        <!--                            route-name="dashboard"-->
+                        <!--                        >-->
+                        <!--                            <Home class="h-4 w-4" />-->
+                        <!--                            Dashboard-->
+                        <!--                        </MenuItem>-->
                         <MenuItem
                             route-name="babies.show"
                         >
-                            <Baby class="h-4 w-4" />
+                            <Baby class="h-4 w-4"/>
                             Babies
-                        </MenuItem><MenuItem
+                        </MenuItem>
+                        <MenuItem
                             route-name="baby_actions.show"
                         >
-                            <BookOpen class="h-4 w-4" />
+                            <BookOpen class="h-4 w-4"/>
                             Baby Actions
                         </MenuItem>
                     </nav>
@@ -45,73 +49,70 @@ import {Link} from '@inertiajs/vue3';
         </div>
         <div class="flex flex-col">
             <header class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-                <Sheet>
+                <Sheet v-model:open="sheetOpen">
                     <SheetTrigger as-child>
                         <Button
                             variant="outline"
                             size="icon"
                             class="shrink-0 md:hidden"
                         >
-                            <Menu class="h-5 w-5" />
+                            <Menu class="h-5 w-5"/>
                             <span class="sr-only">Toggle navigation menu</span>
                         </Button>
                     </SheetTrigger>
                     <SheetContent side="left" class="flex flex-col">
                         <nav class="grid gap-2 text-lg font-medium">
                             <MenuItem
-                                route-name="dashboard"
+                                route-name="babies.show"
+                                :on-success="() => {sheetOpen = false}"
                             >
-                                <Home class="h-4 w-4" />
-                                Dashboard
+                                <Baby class="h-4 w-4"/>
+                                Babies
                             </MenuItem>
                             <MenuItem
-                                route-name="babies.show"
+                                :on-success="() => {sheetOpen = false}"
+                                route-name="baby_actions.show"
                             >
-                                <Baby class="h-4 w-4" />
-                                Babies
-                            </MenuItem><MenuItem
-                            route-name="baby_actions.create"
-                        >
-                            <BookOpen class="h-4 w-4" />
-                            Baby Actions
-                        </MenuItem>
+                                <BookOpen class="h-4 w-4"/>
+                                Baby Actions
+                            </MenuItem>
                         </nav>
                     </SheetContent>
                 </Sheet>
                 <div class="w-full flex-1">
-<!--                    <form>-->
-<!--                        <div class="relative">-->
-<!--                            <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />-->
-<!--                            <Input-->
-<!--                                type="search"-->
-<!--                                placeholder="Search products..."-->
-<!--                                class="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"-->
-<!--                            />-->
-<!--                        </div>-->
-<!--                    </form>-->
+                    <!--                    <form>-->
+                    <!--                        <div class="relative">-->
+                    <!--                            <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />-->
+                    <!--                            <Input-->
+                    <!--                                type="search"-->
+                    <!--                                placeholder="Search products..."-->
+                    <!--                                class="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"-->
+                    <!--                            />-->
+                    <!--                        </div>-->
+                    <!--                    </form>-->
                 </div>
                 <span class="text-sm">Hi, {{ $page.props.auth.user.name }}</span>
                 <Link :href="route('logout')" method="post" as="button" type="button">
-                    <LogOut />
+                    <LogOut/>
                 </Link>
-<!--                <DropdownMenu>-->
-<!--                    <DropdownMenuTrigger as-child>-->
-<!--                        <Button variant="secondary" size="icon" class="rounded-full">-->
-<!--                            <CircleUser class="h-5 w-5" />-->
-<!--                            <span class="sr-only">Toggle user menu</span>-->
-<!--                        </Button>-->
-<!--                    </DropdownMenuTrigger>-->
-<!--                    <DropdownMenuContent align="end">-->
-<!--                        <DropdownMenuLabel>My Account</DropdownMenuLabel>-->
-<!--                        <DropdownMenuSeparator />-->
-<!--                        <DropdownMenuItem>Settings</DropdownMenuItem>-->
-<!--                        <DropdownMenuItem>Support</DropdownMenuItem>-->
-<!--                        <DropdownMenuSeparator />-->
-<!--                        <DropdownMenuItem>-->
-<!--                            <Link :href="route('logout')" method="post" as="button" type="button">Logout</Link>-->
-<!--                        </DropdownMenuItem>-->
-<!--                    </DropdownMenuContent>-->
-<!--                </DropdownMenu>-->
+                <!--                <DropdownMenu>-->
+                <!--                    <DropdownMenuTrigger as-child>-->
+                <!--                        <Button variant="secondary" size="icon" class="rounded-full">-->
+                <!--                            <CircleUser class="h-5 w-5" />-->
+                <!--                            <span class="sr-only">Toggle user menu</span>-->
+                <!--                        </Button>-->
+                <!--                    </DropdownMenuTrigger>-->
+                <!--                    <DropdownMenuContent align="end">-->
+                <!--                        <DropdownMenuLabel>My Account</DropdownMenuLabel>-->
+                <!--                        <DropdownMenuSeparator />-->
+                <!--                        <DropdownMenuItem>Settings</DropdownMenuItem>-->
+                <!--                        <DropdownMenuItem>Support</DropdownMenuItem>-->
+                <!--                        <DropdownMenuSeparator />-->
+                <!--                        <DropdownMenuItem>-->
+                <!--                            <Link :href="route('logout')" method="post" as="button" type="button">Logout</Link>-->
+                <!--                        </DropdownMenuItem>-->
+                <!--                    </DropdownMenuContent>-->
+                <!--                </DropdownMenu>-->
             </header>
             <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 container">
                 <div class="flex items-center">
@@ -119,7 +120,7 @@ import {Link} from '@inertiajs/vue3';
                     </h1>
                 </div>
                 <div class="flex flex-1 rounded-lg">
-                        <slot/>
+                    <slot/>
                 </div>
             </main>
         </div>
