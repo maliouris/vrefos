16bf4f30c4649f4752e284d5272d10faa4383c24
Ioannis Maliouris <giannism@e-avenue.eu>
2024-12-01 18:06:13 +0200

    refactor: introduce more abstractions over push notifications

--- app/Contracts/PushNotifications.php
+++ app/Contracts/PushNotifications.php
@@ -2,8 +2,16 @@
 
 namespace App\Contracts;
 
-use App\Models\BabyAction;
+use App\Models\User;
 
 interface PushNotifications {
-    public function sendBabyActionReminder(BabyAction $babyAction): void;
+    /**
+     * @param string $title
+     * @param string $url
+     * @param non-empty-array<'web'|'android'|'ios'> $devices
+     * @param non-empty-array<User> $users
+     * @param string $body
+     * @return void
+     */
+    public function send(string $title, string $url, array $devices, array $users, string $body = ''): void;
 }
--- app/Services/BeamsNotificationsService.php
+++ app/Services/BeamsNotificationsService.php
@@ -4,25 +4,29 @@ namespace App\Services;
 
 use App\Contracts\BeamsClient;
 use App\Contracts\PushNotifications;
-use App\Models\BabyAction;
-use Illuminate\Routing\UrlGenerator;
 
 readonly class BeamsNotificationsService implements PushNotifications {
-    public function __construct(private BeamsClient $client, private UrlGenerator $urlGenerator)
+    public function __construct(private BeamsClient $client)
     {
     }
 
-    public function sendBabyActionReminder(BabyAction $babyAction): void {
-        $action = strtolower($babyAction->babyActionType->name);
-
-        $this->client->publishToUsers([strval($babyAction->baby->user->id)], [
-            "web" => [
-                "notification" => [
-                    "title" => "Ready for the waaah concert?",
-                    "body" => "Tick-tock! You baby {$babyAction->baby->name} should {$action}. It's been almost 3 hours since the last time.",
-                    "deep_link" => $this->urlGenerator->to('/')
+    public function send(string $title, string $url, array $devices, array $users, string $body = ''): void
+    {
+        $payload = array_map(function ($device) use ($title, $body, $url) {
+            return [
+                $device => [
+                    "notification" => [
+                        "title" => $title,
+                        "body" => $body,
+                        "deep_link" => $url
+                    ]
                 ]
-            ]
-        ]);
+            ];
+        }, $devices);
+
+        if (!empty($users)) {
+            $beamsUsers = array_map(fn ($user) => strval($user->id), $users);
+            $this->client->publishToUsers($beamsUsers, $payload);
+        }
     }
 }
